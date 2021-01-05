37b2124daed871c9e5cded0fbf5502796700022e
limunosekai <powerlsh0103@gmail.com>
2021-01-06 01:50:44 +0900

    Add copy func

--- 1-color-flipper/assets/js/hex.js
+++ 1-color-flipper/assets/js/hex.js
@@ -1,7 +1,8 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const btn = document.querySelector('.btn');
+const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
+const copy = document.getElementById('copy');
 
 btn.addEventListener('click', function(){
   let hexColor = '#';
@@ -12,7 +13,21 @@ btn.addEventListener('click', function(){
   color.textContent = hexColor;
 });
 
+copy.addEventListener('click', copyColor);
+
 function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
+}
+
+function copyColor() {
+  let text = color.innerText;
+  const createInput = document.createElement('input');
+  createInput.setAttribute('type', 'text');
+  color.appendChild(createInput);
+  createInput.value = text;
+  createInput.select();
+  document.execCommand('copy');
+  color.removeChild(createInput);
+  pop.classList.add('showing');
 }
 
