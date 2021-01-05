a35ae6f204cbddf10f95967031ce2b06a26aca79
limunosekai <powerlsh0103@gmail.com>
2021-01-06 01:14:08 +0900

    Finish color-flipper

--- 1-color-flipper/assets/js/app.js
+++ 1-color-flipper/assets/js/app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.querySelector('.btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+  // 0 - 3의 랜덤 숫자
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
